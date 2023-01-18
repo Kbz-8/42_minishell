42df2d5adf208ba29eb2102eb0da5dc478cca0e7
Valentin Vaas <vvaas@3D3.42angouleme.fr>
2023-01-18 12:37:34 +0100

    user_input to readline

--- src/core/user_input.c
+++ src/core/user_input.c
@@ -6,23 +6,13 @@
 /*   By: vvaas <vvaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 11:15:00 by vvaas             #+#    #+#             */
-/*   Updated: 2023/01/18 12:36:12 by vvaas            ###   ########.fr       */
+/*   Updated: 2023/01/18 12:37:31 by vvaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <utils.h>
-#include <libft.h>
-#include <unistd.h>
-#include <memory.h>
 #include <readline/readline.h>
-#include <readline/history.h>
-
-#define BUFFER_SIZE 2048
 
 char	*user_input(void)
 {
-	char *input;
-
-	input = readline(NULL);
-	return (input);
+	return (readline(NULL));
 }
