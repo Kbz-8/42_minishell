28f4387ddc3989c51329ad35b3ef8e09b72ab133
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-01-18 12:44:42 +0100

    env

--- include/nexus.h
+++ include/nexus.h
@@ -6,7 +6,7 @@
 /*   By: maldavid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 11:59:38 by maldavid          #+#    #+#             */
-/*   Updated: 2023/01/18 12:02:24 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/01/18 12:34:48 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,10 @@ typedef struct	s_parser_info
 	t_command	command;
 	const char	*args;
 }	t_parser_info;
+
+typedef struct	s_env
+{
+	int	last_return;
+}	t_env;
 
 #endif
