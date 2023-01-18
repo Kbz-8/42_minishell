432fed005c6ad56e68019fb2f7d0e3d197825a21
Valentin Vaas <vvaas@3D3.42angouleme.fr>
2023-01-18 12:35:26 +0100

    user_input : gnl to readline

--- src/core/user_input.c
+++ src/core/user_input.c
@@ -6,7 +6,7 @@
 /*   By: vvaas <vvaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 11:15:00 by vvaas             #+#    #+#             */
-/*   Updated: 2023/01/18 11:51:02 by vvaas            ###   ########.fr       */
+/*   Updated: 2023/01/18 12:35:09 by vvaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,15 @@
 #include <libft.h>
 #include <unistd.h>
 #include <memory.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #define BUFFER_SIZE 2048
 
 char	*user_input()
 {
 	char *input;
-	int len_input;
-	char *tmp;
 
-	input = NULL;
-	tmp = kalloc(BUFFER_SIZE, 1);
-	len_input = read(0, tmp, BUFFER_SIZE);
-	if (len_input <= 0)
-	{
-		dealloc(tmp);
-		return (NULL);
-	}
-	while (len_input == 2048)
-	{
-		input = ft_joinfree(input, tmp);
-		len_input = read(0, tmp, BUFFER_SIZE);
-		tmp[len_input] = 0;
-	}
-	input = ft_joinfree(input, tmp);
-	input[ft_strlen(input) - 1] = 0;
+	input = readline(NULL);
 	return (input);
 }
