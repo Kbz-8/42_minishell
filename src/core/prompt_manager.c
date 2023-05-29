afb5e3b6fc6c8fe28ae8db16431ec17a7cafb1d1
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-05-29 17:29:47 +0200

    adding multiline prompt support, fixing AST visitor bug

--- src/core/prompt_manager.c
+++ src/core/prompt_manager.c
@@ -6,17 +6,17 @@
 /*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 16:21:17 by maldavid          #+#    #+#             */
-/*   Updated: 2023/05/28 16:53:32 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/05/29 17:11:25 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft.h>
+#include <stdio.h>
 #include <prompt.h>
 #include <stddef.h>
-#include <libft.h>
 #include <memory.h>
-#include <stdio.h>
-#include <readline/readline.h>
 #include <stdbool.h>
+#include <readline/readline.h>
 
 void	init_prompt(t_prompt *prompt)
 {
@@ -31,9 +31,33 @@ void	init_prompt(t_prompt *prompt)
 	ft_strcpy(prompt->text + 1 + username_size, "@ minishell]$ ");
 }
 
-static void	incomplete_string(char *entry)
+static void	incomplete_string(char **entry)
 {
-	(void)entry;
+	char	*new_line;
+	char	*new_entry;
+	size_t	i;
+	size_t	j;
+	bool	in_string;
+
+	i = 0;
+	in_string = true;
+	new_line = readline("> ");
+	j = ft_strlen(*entry);
+	new_entry = malloc(j + ft_strlen(new_line) + 2);
+	ft_memset(new_entry, 0, j + ft_strlen(new_line) + 2);
+	ft_strcpy(new_entry, *entry);
+	new_entry[j] = '\n';
+	j++;
+	while (new_line[i] != 0)
+	{
+		if (new_line[i] == '"' || new_line[i] == '\'')
+			in_string = !in_string;
+		new_entry[j++] = new_line[i++];
+	}
+	if (in_string)
+		incomplete_string(&new_entry);
+	free(*entry);
+	*entry = new_entry;
 }
 
 char	*display_prompt(t_prompt *prompt)
@@ -52,6 +76,6 @@ char	*display_prompt(t_prompt *prompt)
 		i++;
 	}
 	if (in_string)
-		incomplete_string(entry);
+		incomplete_string(&entry);
 	return (entry);
 }
--- third_party/libft/string/ft_strndup.c
+++ third_party/libft/string/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 14:36:12 by maldavid          #+#    #+#             */
-/*   Updated: 2023/05/14 11:05:27 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/05/29 17:08:31 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strndup(const char *s, t_size size)
 	tmp = (char *)s;
 	while (*tmp)
 		tmp++;
-	ptr = (char *)ft_malloc(size + 1);
+	ptr = (char *)ft_memalloc(size + 1);
 	tmp = ptr;
 	while (size-- && *s && tmp)
 		*tmp++ = *s++;
