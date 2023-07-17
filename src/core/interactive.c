df5bf041b134e8d5744497068b87cf7dd75d076f
Valentin Vaas <vvaas@1G2.42angouleme.fr>
2023-07-17 19:33:01 +0200

    added ctrl c exit code

--- src/core/interactive.c
+++ src/core/interactive.c
@@ -6,7 +6,7 @@
 /*   By: vvaas <vvaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:25:46 by vvaas             #+#    #+#             */
-/*   Updated: 2023/07/01 15:37:02 by vvaas            ###   ########.fr       */
+/*   Updated: 2023/07/17 19:24:25 by vvaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void	process(int sig)
 		ft_putchar('\n');
 		rl_replace_line("", 0);
 		rl_redisplay();
+		get_env_data()->last_return = 130;
 	}
 	if (sig == SIGQUIT)
 		printf("%c%c  %c%c", 8, 8, 8, 8);
