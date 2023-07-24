ff95a5f241108192da67c056762b1232ec6df9e6
Valentin Vaas <vvaas@1E7.42angouleme.fr>
2023-07-24 23:24:29 +0200

    Revert "gg"
    This reverts commit 93eb1a5827d7b68d3f18cb178d5791d954b07495.

--- src/exec/command.c
+++ src/exec/command.c
@@ -6,7 +6,7 @@
 /*   By: vvaas <vvaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 12:06:31 by vvaas             #+#    #+#             */
-/*   Updated: 2023/07/24 22:48:13 by vvaas            ###   ########.fr       */
+/*   Updated: 2023/07/24 20:04:32 by vvaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ bool	check_isdir(t_parser_info *info)
 		get_env_data()->last_return = 126;
 		return (1);
 	}
-	
 	return (0);
 }
 
@@ -71,7 +70,7 @@ void	command(t_parser_info *info)
 		ft_execve(info->cmd.str, (char **)info->args, create_env());
 	else if (!access(info->cmd.str, F_OK))
 	{
-		printf("minishell: %s: Permission denied4\n", info->args[0]);
+		printf("minishell: %s: Permission denied\n", info->args[0]);
 		get_env_data()->last_return = 126;
 	}
 	else
@@ -81,9 +80,99 @@ void	command(t_parser_info *info)
 	}
 }
 
-t_parser_info *redirections(t_parser_info *info)
+bool	is_redir(t_parser_info *info)
 {
-	
+	return ((info->next->link == R_OUT || info->next->link == R_OUT_ABSOLUTE) && info->next->next);
+}
+
+void	closeopen(t_parser_info *info)
+{
+	int fd;
+
+	if (info->link != R_OUT_ABSOLUTE)
+	{
+		fd = open(info->next->args[0], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+		if (fd != -1)
+			close(fd);
+	}
+	else
+	{
+		fd = open(info->next->args[0], O_CREAT | O_APPEND | O_WRONLY, 0644);
+		if (fd != -1)
+			close(fd);
+	}
+}
+
+t_parser_info	*r_out(t_parser_info *info)
+{
+	int save;
+	t_parser_info *tmp;
+	bool perror;
+	int	link;
+
+	perror = 0;
+	tmp = info;
+	while (is_redir(info))
+	{
+		closeopen(info);
+		if (access(info->next->args[0], W_OK) != 0)
+		{
+			if (!perror)
+				printf("minishell: %s: Permission denied\n", info->next->args[0]);	
+			perror = 1;
+		}
+		info = info->next;
+	}
+	save = dup(1);
+	close(1);
+	if (info->link != R_OUT_ABSOLUTE && !perror)
+		open(info->next->args[0], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	else if (!perror)
+		open(info->next->args[0], O_CREAT | O_APPEND | O_WRONLY, 0644);
+	if (access(info->next->args[0], W_OK) != 0)
+	{
+		if (!perror)
+		{
+			dup2(save, 1);
+			close(save);
+			printf("minishell: %s: Permission denied\n", info->next->args[0]);
+			perror = 1;
+		}
+		get_env_data()->last_return = 1;
+	}
+	else
+	{
+		command(tmp);
+		dup2(save, 1);
+		close(save);
+	}
+	info = info->next;
+	link = info->link;
+	tmp->link = link; // link de zelda loul 	
+	tmp->next = info->next;
+	if (tmp->link == NONE)
+		return (tmp->next);
+	return (tmp);
+}
+
+void	r_in(t_parser_info *info)
+{
+	int save;
+	int fd;
+
+	save = dup(0);
+	close(0);
+	fd = open(info->next->args[0], O_RDONLY | O_CREAT);
+	if (access(info->next->args[0], R_OK) != 0)
+	{
+		printf("minishell: %s: Permission denied\n", info->next->args[0]);
+		get_env_data()->last_return = 1;
+		return ;
+	}
+	command(info);
+	dup2(save, 0);
+	close(fd);
+	close(save);
 }
 
 void	c_pipe(t_parser_info *info)
