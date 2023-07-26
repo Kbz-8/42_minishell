b3bd0bfcf7009fb1a13ffcd760bf51b8c8ebaf25
Valentin Vaas <vvaas@1G1.42angouleme.fr>
2023-07-26 15:34:51 +0200

    ff

--- src/exec/builtin/ft_export.c
+++ src/exec/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: vvaas <vvaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 02:29:32 by vvaas             #+#    #+#             */
-/*   Updated: 2023/07/26 15:30:56 by vvaas            ###   ########.fr       */
+/*   Updated: 2023/07/26 15:34:32 by vvaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,6 @@ void	print_args(char **args)
 		ft_printf("declare -x ");
 		while (args[i][j] != '=' && args[i][j])
 			ft_printf("%c", args[i][j++]);
-		if (args[i][j] == '=' && !args[i][j + 1])
-		{
-			ft_printf("=""\n");
-			i++;
-			j = 0;
-			continue ;
-		}
 		if (!args[i][j + 1])
 		{
 			ft_printf("\n");
